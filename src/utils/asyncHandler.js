5db57dc0d7a502622f8f30165b22a63a29cd1c66
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-04-16 00:44:19 +0600

    some routes and controllers configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,9 @@
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(arr));
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
+
 export { asyncHandler };
 
 // another way try catch ====================================
